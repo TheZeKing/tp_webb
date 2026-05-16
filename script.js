b70f59e70d4d8e8305ca8e181205487d6ce4865c
TheZeKing <z.berrada@esisa.ac.ma>
2026-05-16 17:16:17 +0200

    ajout

--- script.js
+++ script.js
@@ -13,5 +13,46 @@ btn.addEventListener("click", function() {
     console.log("Nom: " + nom.value);
     console.log("Email: " + email.value);
     console.log("Password: " + password.value);
-    alert (nom.value + " " + email.value + " " + password.value); 
+
+    if (nom.value === "") {
+        alert("Nom est manquant");
+        return;
+    }
+
+    if (email.value === "") {
+        alert("Email est manquant");
+        return;
+    }
+
+    if (!email.value.includes("@")) {
+        alert("Email doit contenir @");
+        return;
+    }
+
+    if (password.value === "") {
+        alert("Mot de passe est manquant");
+        return;
+    }
+
+    if (password.value.length < 6) {
+        alert("Mot de passe doit contenir au moins 6 caractères");
+        return;
+    }
+
+    if (!/[a-z]/.test(password.value)) {
+        alert("Mot de passe doit contenir une minuscule");
+        return;
+    }
+
+    if (!/[A-Z]/.test(password.value)) {
+        alert("Mot de passe doit contenir une majuscule");
+        return;
+    }
+
+    if (!/[0-9]/.test(password.value)) {
+        alert("Mot de passe doit contenir un chiffre");
+        return;
+    }
+
+    alert(nom.value + " " + email.value + " " + password.value);
 });
